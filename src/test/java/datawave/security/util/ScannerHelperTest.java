210c6209b0af1200877ad7ee8337a2f5133f8c15
Ivan Bella <ivan@bella.name>
2019-12-10 18:08:58 -0500

    Migration to use the AccumuloClient instead of the deprecated Connector

--- src/test/java/datawave/security/util/ScannerHelperTest.java
+++ src/test/java/datawave/security/util/ScannerHelperTest.java
@@ -1,24 +1,19 @@
 package datawave.security.util;
 
-import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.fail;
-
+import com.google.common.collect.Lists;
+import datawave.accumulo.inmemory.InMemoryAccumuloClient;
+import datawave.accumulo.inmemory.InMemoryInstance;
+import datawave.security.iterator.ConfigurableVisibilityFilter;
+import datawave.webservice.common.connection.WrappedAccumuloClient;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Map.Entry;
 import java.util.concurrent.TimeUnit;
-
-import datawave.security.iterator.ConfigurableVisibilityFilter;
-import datawave.webservice.common.connection.WrappedConnector;
+import org.apache.accumulo.core.client.AccumuloClient;
 import org.apache.accumulo.core.client.BatchWriter;
 import org.apache.accumulo.core.client.BatchWriterConfig;
-import org.apache.accumulo.core.client.Connector;
 import org.apache.accumulo.core.client.IteratorSetting;
 import org.apache.accumulo.core.client.Scanner;
-import datawave.accumulo.inmemory.InMemoryInstance;
-import org.apache.accumulo.core.client.security.tokens.PasswordToken;
 import org.apache.accumulo.core.data.Key;
 import org.apache.accumulo.core.data.Mutation;
 import org.apache.accumulo.core.data.Value;
@@ -27,17 +22,20 @@ import org.apache.accumulo.core.security.ColumnVisibility;
 import org.junit.Before;
 import org.junit.Test;
 
-import com.google.common.collect.Lists;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
 
 public class ScannerHelperTest {
     
     public static String TABLE_NAME = "DATA";
-    private Connector mockConnector;
+    private AccumuloClient mockConnector;
     
     @Before
     public void setUp() throws Exception {
         InMemoryInstance instance = new InMemoryInstance();
-        mockConnector = instance.getConnector("root", new PasswordToken(""));
+        mockConnector = new InMemoryAccumuloClient("root", instance);
         mockConnector.securityOperations().changeUserAuthorizations("root", new Authorizations("A", "B", "C", "D", "E", "F", "G", "H", "I"));
         mockConnector.tableOperations().create(TABLE_NAME);
         
@@ -71,7 +69,7 @@ public class ScannerHelperTest {
         List<Key> expectedKeys = Lists.newArrayList(new Key("row", "cf2", "cq1", "A", 1L));
         Value expectedVal = new Value(new byte[0]);
         
-        Scanner scanner = ScannerHelper.createScanner(new WrappedConnector(mockConnector, mockConnector), TABLE_NAME, Arrays.asList(a1, a2, a3));
+        Scanner scanner = ScannerHelper.createScanner(new WrappedAccumuloClient(mockConnector, mockConnector), TABLE_NAME, Arrays.asList(a1, a2, a3));
         for (Entry<Key,Value> entry : scanner) {
             assertFalse("Ran out of expected keys but got: " + entry.getKey(), expectedKeys.isEmpty());
             Key expectedKey = expectedKeys.remove(0);
@@ -91,7 +89,7 @@ public class ScannerHelperTest {
         List<Key> expectedKeys = Lists.newArrayList(new Key("row", "cf2", "cq1", "A", 1L));
         Value expectedVal = new Value(new byte[0]);
         
-        Scanner scanner = ScannerHelper.createScanner(new WrappedConnector(mockConnector, mockConnector), TABLE_NAME, Arrays.asList(a1, a2, a3));
+        Scanner scanner = ScannerHelper.createScanner(new WrappedAccumuloClient(mockConnector, mockConnector), TABLE_NAME, Arrays.asList(a1, a2, a3));
         // Clearing the scan iterators should do nothing to the iterators added by ScannerHelper.createScanner
         scanner.clearScanIterators();
         for (Entry<Key,Value> entry : scanner) {
@@ -113,7 +111,7 @@ public class ScannerHelperTest {
         List<Key> expectedKeys = Lists.newArrayList(new Key("row", "cf2", "cq1", "A", 1L));
         Value expectedVal = new Value(new byte[0]);
         
-        Scanner scanner = ScannerHelper.createScanner(new WrappedConnector(mockConnector, mockConnector), TABLE_NAME, Arrays.asList(a1, a2, a3));
+        Scanner scanner = ScannerHelper.createScanner(new WrappedAccumuloClient(mockConnector, mockConnector), TABLE_NAME, Arrays.asList(a1, a2, a3));
         // Removing the scan iterator should do nothing to the iterators added by ScannerHelper.createScanner
         scanner.removeScanIterator("visibilityFilter10");
         for (Entry<Key,Value> entry : scanner) {
@@ -135,7 +133,7 @@ public class ScannerHelperTest {
         List<Key> expectedKeys = Lists.newArrayList(new Key("row", "cf2", "cq1", "A", 1L));
         Value expectedVal = new Value(new byte[0]);
         
-        Scanner scanner = ScannerHelper.createScanner(new WrappedConnector(mockConnector, mockConnector), TABLE_NAME, Arrays.asList(a1, a2, a3));
+        Scanner scanner = ScannerHelper.createScanner(new WrappedAccumuloClient(mockConnector, mockConnector), TABLE_NAME, Arrays.asList(a1, a2, a3));
         // Removing the scan iterator should do nothing to the iterators added by ScannerHelper.createScanner
         scanner.updateScanIteratorOption("visibilityFilter10", ConfigurableVisibilityFilter.AUTHORIZATIONS_OPT, "A,B,C");
         scanner.updateScanIteratorOption("visibilityFilter11", ConfigurableVisibilityFilter.AUTHORIZATIONS_OPT, "A,B,C");
@@ -155,7 +153,7 @@ public class ScannerHelperTest {
         Authorizations a2 = new Authorizations("A", "D", "E");
         Authorizations a3 = new Authorizations("A", "F", "G");
         
-        Scanner scanner = ScannerHelper.createScanner(new WrappedConnector(mockConnector, mockConnector), TABLE_NAME, Arrays.asList(a1, a2, a3));
+        Scanner scanner = ScannerHelper.createScanner(new WrappedAccumuloClient(mockConnector, mockConnector), TABLE_NAME, Arrays.asList(a1, a2, a3));
         // Removing the scan iterator should do nothing to the iterators added by ScannerHelper.createScanner
         scanner.removeScanIterator("sys_visibilityFilter10");
         fail("Removing the scan iterator should have thrown an exception.");
@@ -168,7 +166,7 @@ public class ScannerHelperTest {
         Authorizations a2 = new Authorizations("A", "D", "E");
         Authorizations a3 = new Authorizations("A", "F", "G");
         
-        Scanner scanner = ScannerHelper.createScanner(new WrappedConnector(mockConnector, mockConnector), TABLE_NAME, Arrays.asList(a1, a2, a3));
+        Scanner scanner = ScannerHelper.createScanner(new WrappedAccumuloClient(mockConnector, mockConnector), TABLE_NAME, Arrays.asList(a1, a2, a3));
         // Removing the scan iterator should do nothing to the iterators added by ScannerHelper.createScanner
         scanner.updateScanIteratorOption("sys_visibilityFilter10", ConfigurableVisibilityFilter.AUTHORIZATIONS_OPT, "A,B,C");
         fail("Updating the scan iterator should have thrown an exception.");
@@ -181,7 +179,7 @@ public class ScannerHelperTest {
         Authorizations a2 = new Authorizations("A", "D", "E");
         Authorizations a3 = new Authorizations("A", "F", "G");
         
-        Scanner scanner = ScannerHelper.createScanner(new WrappedConnector(mockConnector, mockConnector), TABLE_NAME, Arrays.asList(a1, a2, a3));
+        Scanner scanner = ScannerHelper.createScanner(new WrappedAccumuloClient(mockConnector, mockConnector), TABLE_NAME, Arrays.asList(a1, a2, a3));
         // Removing the scan iterator should do nothing to the iterators added by ScannerHelper.createScanner
         IteratorSetting cfg = new IteratorSetting(10, "dwSystem_mySystemIterator", ConfigurableVisibilityFilter.class);
         scanner.addScanIterator(cfg);
